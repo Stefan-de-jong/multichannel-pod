a6597fa588233a420a86b8b952d7e6930064eaf5
Darynazar <63154066+Darynazar@users.noreply.github.com>
2021-05-10 13:40:29 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ define('LARAVEL_START', microtime(true));
 | If the application is in maintenance / demo mode via the "down" command
 | we will load this file so that any pre-rendered content can be shown
 | instead of starting the framework, which could cause an exception.
-|
+| Test
 */
 
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
