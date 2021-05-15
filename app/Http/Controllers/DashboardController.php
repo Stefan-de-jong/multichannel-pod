4de1a05af752085969924eeb217a4784eb8b317a
niek <niekdejong1992@gmail.com>
2021-05-15 20:15:25 +0200

    Added empty dashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+    /**
+     * DashboardController constructor.
+     *
+     */
+    public function __construct()
+    {
+        
+    }
+    public function index(){
+
+    }
+}
